6b7630a446018c6032d488e82e88c5fc031f3d18
Malek8910 <malekhassan8910@gmail.com>
2025-05-28 01:11:38 +0300

    new server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const buildPath = path.join(__dirname, 'build');
 
-// Serve static files from the build directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(buildPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
